20ac6aa6f2666ee54c65506746b49fd2ea8dd208
Dhruv Kapoor <dhruvkapoor200210@gmail.com>
2025-09-29 17:30:32 +0530

    merged remote changes

--- src/App.jsx
+++ src/App.jsx
@@ -62,41 +62,41 @@ const App = () => {
       </nav>
 
       <section className="img_display">
-      <div id="mybannerCarousel" className="carousel slide" data-bs-ride="carousel">
+        <div id="mybannerCarousel" className="carousel slide" data-bs-ride="carousel">
 
-        <div className="carousel-inner">
-          {banners.map((banner, index) => (
-            <div key={index} className={`carousel-item ${index === 0 ? "active" : ""}`}>
-              <img
-                src={banner.img}
-                className="d-block w-100 img-fluid"
-                alt={banner.alt}
-                style={{ objectPosition: banner.position }}
-              />
+          <div className="carousel-inner">
+            {banners.map((banner, index) => (
+              <div key={index} className={`carousel-item ${index === 0 ? "active" : ""}`}>
+                <img
+                  src={banner.img}
+                  className="d-block w-100 img-fluid"
+                  alt={banner.alt}
+                  style={{ objectPosition: banner.position }}
+                />
 
-              <div className="carousel-caption text-light">
-                <div className="play">
-                  <button><i className="fa-solid fa-play"></i> Play</button>
-                </div>
-                <div className="info">
-                  <button><i className="fa-solid fa-circle-info"></i> Info</button>
+                <div className="carousel-caption text-light">
+                  <div className="play">
+                    <button><i className="fa-solid fa-play"></i> Play</button>
+                  </div>
+                  <div className="info">
+                    <button><i className="fa-solid fa-circle-info"></i> Info</button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+
+          <button className="carousel-control-prev" type="button" data-bs-target="#mybannerCarousel" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#mybannerCarousel" data-bs-slide="next">
+            <span className="carousel-control-next-icon"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+
         </div>
-
-        <button className="carousel-control-prev" type="button" data-bs-target="#mybannerCarousel" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#mybannerCarousel" data-bs-slide="next">
-          <span className="carousel-control-next-icon"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-
-      </div>
-    </section>
+      </section>
 
       <section className="mid_section mt-5">
         <h2>Must Watch</h2>
