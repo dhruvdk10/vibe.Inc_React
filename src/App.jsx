9c0f2a3701eb08b0839b8a13471e673af21caf2b
Dhruv Kapoor <dhruvkapoor200210@gmail.com>
2025-09-30 21:50:42 +0530

    lu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import DialogueBox from "./Components/DialogueBox";
-import Navbar from "./Components/Navbar";
-import Content from "./Components/Content";
 import Footer from "./Components/Footer";
-import { BrowserRouter as Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import TVShows from "./Pages/TV Shows";
 import Movies from "./Pages/Movies";
@@ -13,25 +8,23 @@ import Trending from "./Pages/Trending";
 import MyList from "./Pages/My List";
 import PageNotFound from "./Pages/Page Not Found";
 
-
 const App = () => {
   return (
-    <><Router>
+    <BrowserRouter basename="/vibe.Inc_React">
       <Navbar />
       <Routes>
-        <Route element={<Home />} path="/"></Route>
-        <Route element={<TVShows />} path="/TVShows"></Route>
-        <Route element={<Movies />} path="/Movies"></Route>
-        <Route element={<Games />} path="/Games"></Route>
-        <Route element={<Trending />} path="/Trending"></Route>
-        <Route element={<MyList />} path="/My List"></Route>
-        <Route element={<PageNotFound />} path="*"></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/TV Shows" element={<TVShows />} />
+        <Route path="/Movies" element={<Movies />} />
+        <Route path="/Games" element={<Games />} />
+        <Route path="/Trending" element={<Trending />} />
+        <Route path="/My List" element={<MyList />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
-    </Router>
       <DialogueBox />
       <Content />
       <Footer />
-    </>
+    </BrowserRouter>
   );
 };
 
