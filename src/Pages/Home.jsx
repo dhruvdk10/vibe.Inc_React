e8ff6b962f2be7850b98bcf07709338aee653e19
Dhruv Kapoor <dhruvkapoor200210@gmail.com>
2025-10-03 20:16:18 +0530

    lu

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import Cards from './Cards';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import Cards from "./Components/Cards";
 import {
   banners,
   mustWatchData,
@@ -8,7 +8,7 @@ import {
   thrillingchillsData,
   cheerfulcomedyData,
   toppicksforyouData
-} from './Data';
+} from "./Components/Data";
 
 
 export const Home = () => {
