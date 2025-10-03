03bc8df8640c1cdf90046ab402620d304d552800
Dhruv Kapoor <dhruvkapoor200210@gmail.com>
2025-10-03 19:09:03 +0530

    lu

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Cards = ({ img, alt, position }) => {
   return (
     <div className="col-6 col-md-2">
-      <div className="card border-0">
+      <div className="movie-card border-0">
         <img
           src={img}
           alt={alt}
-          className="card-img-top img-fluid"
+          className="movie-card-img-top img-fluid"
           style={{ objectPosition: position }}
         />
       </div>
