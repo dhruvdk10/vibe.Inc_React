1f122f246ef988edf4377912e016579c7d076323
Dhruv Kapoor <dhruvkapoor200210@gmail.com>
2025-10-06 23:36:55 +0530

    lu

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Cards = ({ img, alt, position }) => {
   return (
-    <div className="col-6 col-md-2">
+    <div className="col-6 col-lg-2">
       <div className="movie-card border-0">
         <img
           src={img}
