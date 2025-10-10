48c3a3925dcd89a7ddc865b10a6a0701409b48db
Dhruv Kapoor <dhruvkapoor200210@gmail.com>
2025-10-10 16:44:46 +0530

    lu

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,6 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-
 import Cards from "../Components/Cards";
 import {
   banners,
