017d434651800cbc6da4302eac996de580f4ca56
Dhruv Kapoor <dhruvkapoor200210@gmail.com>
2025-10-16 00:47:48 +0530

    lu

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -22,41 +22,41 @@ const Footer = () => {
   ];
   return (
     <footer className="w-100 py-4" style={{ backgroundColor: "#141414", overflowX: "hidden" }}>
-        <div className="d-flex text-center mt-3 mb-5 justify-content-center mx-0">
-          <div className="col-6 col-md-3">
-            <a href="#" className="d-block text-decoration-none">Audio Description</a>
-            <a href="#" className="d-block text-decoration-none" style={{ color: "#ccc", margin: "8px 0" }}>Investor Relations</a>
-            <a href="#" className="d-block text-decoration-none" style={{ color: "#ccc", margin: "8px 0" }}>Legal Notices</a>
-          </div>
-
-          <div className="col-6 col-md-3">
-            <a href="#" className="d-block text-decoration-none" style={{ color: "#ccc", margin: "8px 0" }}>Help Centre</a>
-            <a href="#" className="d-block text-decoration-none" style={{ color: "#ccc", margin: "8px 0" }}>Jobs</a>
-            <a href="#" className="d-block text-decoration-none" style={{ color: "#ccc", margin: "8px 0" }}>Cookie Preferences</a>
-          </div>
-
-          <div className="col-6 col-md-3">
-            <a href="#" className="d-block text-decoration-none" style={{ color: "#ccc", margin: "8px 0" }}>Gift Cards</a>
-            <a href="#" className="d-block text-decoration-none" style={{ color: "#ccc", margin: "8px 0" }}>Terms of Use</a>
-            <a href="#" className="d-block text-decoration-none" style={{ color: "#ccc", margin: "8px 0" }}>Corporate Information</a>
-          </div>
-
-          <div className="col-6 col-md-3">
-            <a href="#" className="d-block text-decoration-none" style={{ color: "#ccc", margin: "8px 0" }}>Media Centre</a>
-            <a href="#" className="d-block text-decoration-none" style={{ color: "#ccc", margin: "8px 0" }}>Privacy</a>
-            <a href="#" className="d-block text-decoration-none" style={{ color: "#ccc", margin: "8px 0" }}>Contact Us</a>
-          </div>
+      <div className="d-flex text-center mt-3 mb-5 justify-content-center mx-0">
+        <div className="col-6 col-md-3">
+          <a href="#" className="d-block text-decoration-none">Audio Description</a>
+          <a href="#" className="d-block text-decoration-none" style={{ color: "#ccc", margin: "8px 0" }}>Investor Relations</a>
+          <a href="#" className="d-block text-decoration-none" style={{ color: "#ccc", margin: "8px 0" }}>Legal Notices</a>
         </div>
 
-        <div className="text-center mb-4">
-          <div className="d-flex justify-content-center gap-3 flex-wrap">
-            {storeBadges.map((badge, index) => (
-              <div key={index} className="store-buttons">
-                <img src={badge.src} alt={badge.alt} style={{ height: "40px" }} />
-              </div>
-            ))}
-          </div>
+        <div className="col-6 col-md-3">
+          <a href="#" className="d-block text-decoration-none" style={{ color: "#ccc", margin: "8px 0" }}>Help Centre</a>
+          <a href="#" className="d-block text-decoration-none" style={{ color: "#ccc", margin: "8px 0" }}>Jobs</a>
+          <a href="#" className="d-block text-decoration-none" style={{ color: "#ccc", margin: "8px 0" }}>Cookie Preferences</a>
         </div>
+
+        <div className="col-6 col-md-3">
+          <a href="#" className="d-block text-decoration-none" style={{ color: "#ccc", margin: "8px 0" }}>Gift Cards</a>
+          <a href="#" className="d-block text-decoration-none" style={{ color: "#ccc", margin: "8px 0" }}>Terms of Use</a>
+          <a href="#" className="d-block text-decoration-none" style={{ color: "#ccc", margin: "8px 0" }}>Corporate Information</a>
+        </div>
+
+        <div className="col-6 col-md-3">
+          <a href="#" className="d-block text-decoration-none" style={{ color: "#ccc", margin: "8px 0" }}>Media Centre</a>
+          <a href="#" className="d-block text-decoration-none" style={{ color: "#ccc", margin: "8px 0" }}>Privacy</a>
+          <a href="#" className="d-block text-decoration-none" style={{ color: "#ccc", margin: "8px 0" }}>Contact Us</a>
+        </div>
+      </div>
+
+      <div className="text-center mb-4">
+        <div className="d-flex justify-content-center gap-3 flex-wrap">
+          {storeBadges.map((badge, index) => (
+            <div key={index} className="store-buttons">
+              <img src={badge.src} alt={badge.alt} style={{ height: "40px" }} />
+            </div>
+          ))}
+        </div>
+      </div>
 
 
       <hr className="width-100" style={{ height: "0.2px" }} />
@@ -73,8 +73,8 @@ const Footer = () => {
       <div className="social-icons">
         <div className="footer-links d-flex justify-content-center gap-3 fs-5">
           {socialLinks.map((social, index) => (
-            <a key={index} href={social.href} className="fs-4 me-3">
-              <i className={social.icon}></i>
+            <a key={index} href={social.href} className="fs-4 me-3 text-white">
+              {social.icon}
             </a>
           ))}
         </div>
