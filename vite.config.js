3d2869bc79009e65633c48c15f7f797d605b9cc3
Dhruv Kapoor <dhruvkapoor200210@gmail.com>
2025-10-17 00:44:31 +0530

    lu

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Use an environment variable to choose deployment platform
-// - Firebase: relative paths './'
-// - GitHub Pages: repo subpath '/vibe.Inc_React/'
 const basePath = process.env.VITE_DEPLOY === 'firebase' ? './' : '/vibe.Inc_React/'
-
 export default defineConfig({
   plugins: [react()],
-  base: basePath,
+  base: basePath
 })
-
