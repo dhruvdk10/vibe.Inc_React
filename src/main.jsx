950fcf638abb0054a025dbfb61176af33b9e88e6
Dhruv Kapoor <dhruvkapoor200210@gmail.com>
2025-12-21 17:42:39 +0530

    lu

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from './Components/ContextAPI/ThemeProvider';
 import { UserProvider } from './Components/ContextAPI/UserContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "./index.css";
 import "./Firebase";
 
